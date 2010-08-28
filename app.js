43cf5a4cb7e624b1ece13770c7516cb2f048855a
Michael Nutt <michael@nuttnet.net>
2010-08-27 22:27:54 -0400

    Add mongodb

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ require.paths.unshift('deps/');
 var http = require('http');
 var nodestatic = require('node-static/lib/node-static');
 
+// Mongodb
+require.paths.unshift('deps/node-mongodb-native/lib');
+var Db = require('mongodb').Db;
+var Connection = require('mongodb').Connection;
+var Server = require('mongodb').Server;
+var BSON = require('mongodb').BSONNative;
+
+// Mongodb client
+var client = new Db('nodescape', new Server("127.0.0.1", 27017, {native_parser:true}));
+
+// Static file server
 var file = new(nodestatic.Server)('./public');
 
 http.createServer(function(request, response) {
